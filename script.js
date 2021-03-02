760b3528d0b8380c65ca5b7918b84eec53ad7fd9
Ebony <leon4rdoalvess@gmail.com>
2021-03-01 21:37:42 -0300

    Iniciando script

---
+++ script.js
@@ -0,0 +1,32 @@
+const
+    classX = 'x'
+
+const classCirculo = 'circle'
+const combVence = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [2, 5, 8],
+    [1, 4, 7],
+    [2, 4, 6],
+    [0, 4, 8]
+]
+
+const celulaElement = document.querySelectorAll('[data-celula]')
+const bordaElement = document.getElementById('borda')
+const msg_vencedorElement = document.getElementById('msgvencedor')
+const bt_reiniciar = document.getElementById('bt_reiniciar')
+const msg_vencedor_text = document.querySelector(['data-msg_vencedor'])
+
+let circleTurn
+
+iniciar()
+bt_reiniciar.addEventListener('click', iniciar)
+
+function iniciar(){
+    
+
+
+
+}
